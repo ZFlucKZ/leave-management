40b32e5b7633452f021fa2dfe0102614ad564220
Pajjaphon Whanchid <97209810+ZFlucKZ@users.noreply.github.com>
2023-11-03 23:28:44 +0700

    Create : Button components

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Button from '~/features/ui/components/Button';
+
+const IndexPage = () => {
+  return (
+    <>
+      <Button color="primary" align="center">
+        Hello
+      </Button>
+    </>
+  );
+};
+
+export default IndexPage;
---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import { type Config } from 'tailwindcss';
+import colors from 'tailwindcss/colors';
+
+export default {
+  content: ['./src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.blue,
+        secondary: colors.sky,
+        success: colors.green,
+        danger: colors.red,
+        info: colors.cyan,
+        warn: colors.yellow,
+        default: colors.gray,
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;

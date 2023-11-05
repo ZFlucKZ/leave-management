16622577d1ef3dcfff12980cbc2879a6d712a723
Pajjaphon Whanchid <97209810+ZFlucKZ@users.noreply.github.com>
2023-11-05 17:58:47 +0700

    Create : Prisma migration

--- src/features/announcements/components/AnnouncementItem.tsx
+++ src/features/announcements/components/AnnouncementItem.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
-import { type Announcement } from '../types';
+import { type AnnouncementItem } from '../types';
 
-export type ArticleItemProps = Announcement;
+export type ArticleItemProps = AnnouncementItem;
 
 const AnnouncementItem = ({ slug, title }: ArticleItemProps) => {
   return <Link href={`/announcements/${slug}`}>{title}</Link>;
--- src/server/api/routers/announement.ts
+++ src/server/api/routers/announement.ts
@@ -1,64 +1,12 @@
 import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
+import { slugify } from '~/features/shared/helpers/slugify';
 
 import {
   createTRPCRouter,
   protectedProcedure,
   publicProcedure,
 } from '~/server/api/trpc';
-
-interface Announcement {
-  id: number;
-  slug: string;
-  title: string;
-  content: string;
-  excerpt: string;
-}
-
-const announcements: Announcement[] = [
-  {
-    id: 1,
-    slug: 'slug-1',
-    title: 'Title#1',
-    excerpt: 'Excerpt#1',
-    content: 'Content#1',
-  },
-  {
-    id: 2,
-    slug: 'slug-2',
-    title: 'Title#2',
-    excerpt: 'Excerpt#2',
-    content: 'Content#2',
-  },
-  {
-    id: 3,
-    slug: 'slug-3',
-    title: 'Title#3',
-    excerpt: 'Excerpt#3',
-    content: 'Content#3',
-  },
-  {
-    id: 4,
-    slug: 'slug-4',
-    title: 'Title#4',
-    excerpt: 'Excerpt#4',
-    content: 'Content#4',
-  },
-  {
-    id: 5,
-    slug: 'slug-5',
-    title: 'Title#5',
-    excerpt: 'Excerpt#5',
-    content: 'Content#5',
-  },
-  {
-    id: 6,
-    slug: 'slug-6',
-    title: 'Title#6',
-    excerpt: 'Excerpt#6',
-    content: 'Content#6',
-  },
-];
 
 // * Validate by zod
 // * number => z.number()
@@ -67,11 +15,96 @@ const announcements: Announcement[] = [
 //** frontend => api.article.[list, byId, update, delete, add]
 //** backend
 export const announcementRouter = createTRPCRouter({
-  list: publicProcedure.query(() => {
+  list: publicProcedure.query(async ({ ctx }) => {
+    const announcements = ctx.db.announcement.findMany({
+      select: {
+        id: true,
+        slug: true,
+        title: true,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+
     return announcements;
   }),
 
-  bySlug: publicProcedure.input(z.string()).query(({ input }) => {
-    return announcements.find((announcement) => announcement.slug === input);
+  byId: publicProcedure.input(z.number()).query(async ({ input, ctx }) => {
+    const announcement = await ctx.db.announcement.findUnique({
+      where: {
+        id: input,
+      },
+      select: {
+        title: true,
+      },
+    });
+
+    if (!announcement) throw new TRPCError({ code: 'NOT_FOUND' });
+
+    return announcement;
+  }),
+
+  bySlug: publicProcedure.input(z.string()).query(async ({ input, ctx }) => {
+    const announcement = await ctx.db.announcement.findUnique({
+      where: {
+        slug: input,
+      },
+      select: {
+        id: true,
+        title: true,
+        slug: true,
+      },
+    });
+
+    if (!announcement) throw new TRPCError({ code: 'NOT_FOUND' });
+
+    return announcement;
+  }),
+
+  add: publicProcedure
+    .input(
+      z.object({
+        title: z.string(),
+        excerpt: z.string(),
+        content: z.string(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const announcement = await ctx.db.announcement.create({
+        data: { ...input, slug: slugify(input.title), userId: 1 },
+      });
+
+      return announcement;
+    }),
+
+  update: publicProcedure
+    .input(
+      z.object({
+        id: z.number(),
+        data: z.object({
+          title: z.string(),
+          excerpt: z.string(),
+          content: z.string(),
+        }),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const announcement = await ctx.db.announcement.update({
+        where: {
+          id: input.id,
+        },
+        data: input.data.title
+          ? { ...input, slug: slugify(input.data.title) }
+          : input.data,
+      });
+
+      return announcement;
+    }),
+
+  remove: publicProcedure.input(z.number()).mutation(async ({ input, ctx }) => {
+    await ctx.db.announcement.delete({
+      where: { id: input },
+    });
   }),
 });
